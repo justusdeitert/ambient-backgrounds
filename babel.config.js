14c9e9269e4de99921196bc50f9ed37d54a6d6c7
Justus Deitert <mail@justusdeitert.de>
2018-11-14 19:52:18 +0100

    Scaffold project with webpack and babel
    - Configure webpack 4 with dev server and production build
    - Set up babel 7 with preset-env and dynamic import plugin
    - Add css and js module loaders

---
+++ babel.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  presets: [
+    ['@babel/preset-env', {
+      targets: '> 0.25%, not dead',
+    }],
+  ],
+  plugins: [
+    '@babel/plugin-syntax-dynamic-import',
+  ],
+};
