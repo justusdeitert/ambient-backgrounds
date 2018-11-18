d478eb9633e1edc5053e1f76f7b68b3de2051d40
Justus Deitert <mail@justusdeitert.de>
2018-11-18 10:12:16 +0000

    Switch to dynamic imports for effects
    - Define effect list with lazy loaders per chunk
    - Add loadEffect function with cleanup lifecycle
    - Use webpack chunk names for readable output files

--- src/index.js
+++ src/index.js
@@ -1,4 +1,29 @@
 import './styles/main.css';
-import { init } from './effects/swirl';
 
-init('.content--canvas');
+const effectList = [
+  { name: 'drift',    label: 'Drift',    loader: () => import(/* webpackChunkName: "swirl" */ './effects/swirl') },
+  { name: 'circuit',  label: 'Circuit',  loader: () => import(/* webpackChunkName: "pipeline" */ './effects/pipeline') },
+  { name: 'nebula',   label: 'Nebula',   loader: () => import(/* webpackChunkName: "shift" */ './effects/shift') },
+  { name: 'vortex',   label: 'Vortex',   loader: () => import(/* webpackChunkName: "coalesce" */ './effects/coalesce') },
+  { name: 'borealis', label: 'Borealis', loader: () => import(/* webpackChunkName: "aurora" */ './effects/aurora') },
+];
+
+let currentCleanup = null;
+
+function loadEffect(index) {
+  if (currentCleanup) {
+    currentCleanup();
+    currentCleanup = null;
+  }
+
+  const effect = effectList[index];
+
+  const container = document.querySelector('.content--canvas');
+  container.innerHTML = '';
+
+  effect.loader().then(mod => {
+    currentCleanup = mod.init('.content--canvas');
+  });
+}
+
+loadEffect(0);
